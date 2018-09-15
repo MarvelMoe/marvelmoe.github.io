7662a916ee3894834d547b51b7cad20d6a4c3412
Moe <marvel451@aol.com>
2018-09-15 00:21:55 -0700

    Added social icon fonts

--- portfolio/src/Connect.js
+++ portfolio/src/Connect.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 const Connect = () => (
 
 		<div> 
-			<h2>Let's Connect </h2>  
+			<h2>Let's Connect </h2> 
+			 <a href="mailto:moe.himed@gmail.com" className=" glyph" target="_blank">A</a> 
+ 			 <a href="https://www.facebook.com/moe.himed" className="glyph" target="_blank">B</a>
+ 			 <a href="https://twitter.com/MoeHimed" className="glyph" target="_blank">C</a>
+ 			 <a href="https://www.linkedin.com/in/moe-himed-" className=" glyph" target="_blank">D</a>
+ 	    	 <a href="https://github.com/MarvelMoe" className="glyph" target="_blank">E</a>
 		</div>
 		 
 )
--- portfolio/src/Header.js
+++ portfolio/src/Header.js
@@ -10,14 +10,15 @@ class Header extends React.Component {
 
            <div className="header-wrapper">
              <header className="parent-nav">
+
                <div className="mobile-wrapper">
                   <div className="mobile-logo">
                         <img src={logo} className="mobile-logo" alt="logo" />
                   </div>
-                  <div class="mobile-btn-wrap">
-                    <button class="mobile-btn">Menu</button>
-                    <div class="mobile-content">
-                       <NavLink exact to="/">Home</NavLink>
+                  <div className="mobile-btn-wrap">
+                    <button className="mobile-btn">Menu</button>
+                    <div className="mobile-content">
+                       <NavLink exact to="/" className="home-img" >Home</NavLink>
                        <NavLink to="/about">About</NavLink>
                        <NavLink to="/work">Work</NavLink>
                        <NavLink to="/connect">Connect</NavLink>
@@ -26,10 +27,10 @@ class Header extends React.Component {
                </div>
 
                 <ul className="left-nav nav">
-                 <li className="nav-item"><NavLink exact to="/">Home</NavLink></li>
+                 <li className="nav-item home-img"><NavLink exact to="/">Home</NavLink></li>
                  <li className="nav-item"><NavLink to="/about">About</NavLink></li>
                 </ul>
-                   <img src={logo} className="desktop-logo desktop-logo" alt="logo" />
+                   <img src={logo} className="desktop-logo" alt="logo" />
                 <ul className="right-nav nav">
                  <li className="nav-item"><NavLink to="/work">Work</NavLink></li>
                  <li className="nav-item"><NavLink to="/connect">Connect</NavLink></li>
