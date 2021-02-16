497510d7cb28d4a3d181502f766100fa3f34ecb2
Mohamed Himed <marvel451@aol.com git config --global user.name Moe Himed git config --global user.email marvel451@aol.com>
2021-02-15 22:50:24 -0800

    removed some works

--- portfolio/src/data/myworks.js
+++ portfolio/src/data/myworks.js
@@ -13,41 +13,41 @@ const myworks = [
 		img: "./images/icabayarea.jpg"
 	},
 	{
-		link: "https://codepen.io/MarvelMoe/pen/xjXXGv",
-		title: "React Weather App",
-		description: "Using Open Weather API and Geolocation to get the local weather",
-		img: "./images/react-weather.jpg"
-	},
-	{
 		link: "https://www.isebfremont.org/",
 		title: "Fremont Community Center",
 		description: "Initially built theme and launched the site",
 		img: "./images/isebfremont.jpg"
 	},
 	{
-		link: "https://himedpay.com/",
-		title: "Rent Payment System",
-		description: "Using Stripe's API, tenants can pay their rent",
-		img: "./images/himed-pay.jpg"
-	},
+	// 	link: "https://himedpay.com/",
+	// 	title: "Rent Payment System",
+	// 	description: "Using Stripe's API, tenants can pay their rent",
+	// 	img: "./images/himed-pay.jpg"
+	// },
 	{
 		link: "https://codepen.io/MarvelMoe/pen/BYmpQX/",
 		title: "Black Panther App",
 		description: "Using Marvel's Api and Fetch, find any comic featuring Black Panther",
 		img: "./images/black-panther.jpg"
 	},
+	// {
+	// 	link: "http://marvelmoe.com/wordpress-reactified/",
+	// 	title: "React with WordPress Backend",
+	// 	description: "Remove the word reactified from url to see original WordPress site",
+	// 	img: "./images/react-wordpress.jpg"
+	// },
 	{
-		link: "http://marvelmoe.com/wordpress-reactified/",
-		title: "React with WordPress Backend",
-		description: "Remove the word reactified from url to see original WordPress site",
-		img: "./images/react-wordpress.jpg"
+		link: "https://codepen.io/MarvelMoe/pen/xjXXGv",
+		title: "React Weather App",
+		description: "Using Open Weather API and Geolocation to get the local weather",
+		img: "./images/react-weather.jpg"
 	},
-	{
-		link: "https://transitionzero.com.au/",
-		title: "Bike Rental Store",
-		description: "Setup WooCommerce on this WordPress site",
-		img: "./images/transitionzero.jpg"
-	},
+	// {
+	// 	link: "https://transitionzero.com.au/",
+	// 	title: "Bike Rental Store",
+	// 	description: "Setup WooCommerce on this WordPress site",
+	// 	img: "./images/transitionzero.jpg"
+	// },
 	{
 		link: "https://codepen.io/MarvelMoe/pen/LWoXrQ",
 		title: "SCSS/CSS NBA Jersey",
